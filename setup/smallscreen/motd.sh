3790db44bbdd7ce1969fd3900cec28e48c055e32
Dennis Perry Jr <67666507+UghDamnitDennis@users.noreply.github.com>
2022-04-24 04:44:09 -0500

    Add files via upload

---
+++ setup/smallscreen/motd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Colors
+CLR=$(echo -e '\033[0m')
+BLK=$(echo -e '\033[30m')
+RED=$(echo -e '\033[31m')
+GRN=$(echo -e '\033[32m')
+YLO=$(echo -e '\033[33m')
+BLU=$(echo -e '\033[34m')
+MAG=$(echo -e '\033[35m')
+CYN=$(echo -e '\033[36m')
+WHT=$(echo -e '\033[37m')
+
+# MOTD
+echo ${RED}'             ____                   _     _ '${CLR}
+echo ${GRN}'            / __ \___  ____  ____  (_)___( )'${CLR}
+echo ${YLO}'           / / / / _ \/ __ \/ __ \/ / ___// '${CLR}
+echo ${BLU}'          / /_/ /  __/ / / / / / / (__  )   '${CLR}
+echo ${MAG}'         /_____/\___/_/ /_/_/ /_/_/____/    '${CLR}
+echo ${MAG}'         ______                              '${CLR}
+echo ${BLU}'        /_  __/__  _________ ___  __  ___  __'${CLR}
+echo ${YLO}'         / / / _ \/ ___/ __ `__ \/ / / / |/_/'${CLR}
+echo ${GRN}'        / / /  __/ /  / / / / / / /_/ />  <  '${CLR}
+echo ${RED}'       /_/  \___/_/  /_/ /_/ /_/\__,_/_/|_|  '${CLR};
