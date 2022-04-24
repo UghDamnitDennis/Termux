9c1f0d4be6ed8eaec60f1cf855ea0ae1dbbedcec
Dennis Perry Jr <67666507+UghDamnitDennis@users.noreply.github.com>
2022-04-24 04:42:05 -0500

    Delete helpers directory

--- helpers/scr_temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Text Style and Color
-CLR=$(echo -e '\033[0m')
-INFO=$(echo -e '\033[3;32m')
-USER=$(echo -e '\033[4;33m')
-OPT=$(echo -e '\033[3\033[4;35m')
-WARN=$(echo -e '\033[1;31;43m')
-URG=$(echo -e '\033[1\033[7;31m')
