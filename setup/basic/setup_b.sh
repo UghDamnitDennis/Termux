2e38172f5d21070e95defd172aa193be138e53e8
Dennis Perry Jr <67666507+UghDamnitDennis@users.noreply.github.com>
2022-04-26 05:07:59 -0500

    Create setup_b.sh

---
+++ setup/basic/setup_b.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Placeholder Message
+echo -ne "\033[31mThis is a placeholder file to retain GitHub file organization.\nThe script for Basic Termux setup has not yet been developed.\n"
+exit
