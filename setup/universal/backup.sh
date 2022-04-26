e3fb947af121cb49d1ea9af45709b92af1373d75
Dennis Perry Jr <67666507+UghDamnitDennis@users.noreply.github.com>
2022-04-26 08:11:53 -0500

    Update backup.sh

--- setup/universal/backup.sh
+++ setup/universal/backup.sh
@@ -1,53 +1,12 @@
 #!/bin/bash
 
-# Text Style and Color
-CLR=$(echo -e '\033[0m')
-INFO=$(echo -e '\033[3;32m')
-USER=$(echo -e '\033[4;33m')
-OPT=$(echo -e '\033[3\033[4;35m')
-WARN=$(echo -e '\033[1;31;43m')
-URG=$(echo -e '\033[1\033[7;31m')
-
-# Check for Existing Backup
-if [ -f "/storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz" ]
-    then
-        echo ${WARN}"An existing backup from today was found on this device."${CLR};sleep 0.25s;
-        while true; do
-            echo ${USER}"How would you like to proceed?"${CLR};
-            echo ${OPT}"1 - Delete existing backup and perform a new backup."${CLR};
-            echo ${OPT}"2 - Backup existin backup and perform a new backup."${CLR};
-            echo ${OPT}"3 - Abort backup script."${CLR};
-            read -p ${USER}"Make a selection:"${CLR} exist_backup
-            case $exist_backup in
-                1) echo ${INFO}"Deleting existing backup."${CLR};sleep 0.25s;
-                   rm -rf /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz;
-                   echo ${INFO}"Performing new backup."${CLR};sleep 0.25s;
-                   tar -zcf /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr;
-                   echo ${INFO}"Backup completed successfully."${CLR};sleep 0.25s;
-                2) echo ${INFO}"Backing up existing backup."${CLR};sleep 0.25s;
-                   if [ -d "/storage/emulated/0/termux/backups/backup-backups/" ]
-                       then
-                           mv --backup=t /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz /storage/emulated/0/termux/backups/backup-backups/;
-                           echo ${INFO}"Performing new backup."${CLR};sleep 0.25s;
-                           tar -zcf /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr;
-                           echo ${INFO}"Backup completed successfully."${CLR};sleep 0.25s;
-                       else
-                           mkdir /storage/emulated/0/termux/backups/backup-backups/;
-                           mv --backup=t /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz /storage/emulated/0/termux/backups/backup-backups/;
-                           echo ${INFO}"Performing new backup."${CLR};sleep 0.25s;
-                           tar -zcf /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr;
-                           echo ${INFO}"Backup completed successfully."${CLR};sleep 0.25s;
-                   fi
-                3) echo ${WARN}"Aborting backup script."${CLR};sleep 0.25s;
-                   exit
-            esac
-        done
-    else
-        echo ${INFO}"Performing Termux backup."${CLR};sleep 0.25s;
-        tar -zcf /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr;
-        echo ${INFO}"Backup completed successfully."${CLR};sleep 0.25s;
-fi
-
-# Exit Message
-echo ${WARN}"Although not required, it is strongly suggested to restart Termux before continuing."${CLR};sleep 0.25s;
+# Backup Termux
+[ -f "/storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz" ] && mkdir /storage/emulated/0/termux/backup_duplicates/;mv /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz /storage/emulated/0/termux/backup_duplicates/;echo -ne "\033[1m\033[4;30;43mAn existing backup for $(date +%m/%d/%Y) was found. It has been moved to 'storage/emulated/0/termux/backup_duplicates'.\033[0m\n" || echo -ne "\033[3m\033[4;33mPerforming Termux backup.\033[0m\n"
+echo -ne "\033[31m>>>>>>>>>>>>>                           [33%]\033[0m\r"
+sleep 3
+echo -ne "\033[31m>>>>>>>>>>>>>>>>>>>>>>>>>>              [66%]\033[0m\r"
+tar -zcf /storage/emulated/0/termux/backups/$(date +%m/%d/%Y)_termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr
+echo -ne "\033[32m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\033[0m\n"
+sleep 1
+echo -ne "\033[1m\033[4;32mBackup completed successfully.\033[0m\n\033[1m\033[4;30;43mAlthought not required, it is recommended to restart Termux before continuing...\033[0m\n"
 exit
