7821cddacf5a98e4ad1d1a576612650835e1d97f
Dennis Perry Jr <67666507+UghDamnitDennis@users.noreply.github.com>
2022-04-26 12:33:48 -0500

    Update update.sh

--- setup/universal/update.sh
+++ setup/universal/update.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-# Text Style and Color
-CLR=$(echo -e '\033[0m')
-INFO=$(echo -e '\033[3;32m')
-USER=$(echo -e '\033[4;33m')
-OPT=$(echo -e '\033[3\033[4;35m')
-WARN=$(echo -e '\033[1;31;43m')
-URG=$(echo -e '\033[1\033[7;31m')
-
 # Check For/Apply Updates
-echo ${INFO}"Updating Termux packages."${CLR};sleep 0.25s;
+echo -ne "\033[3m\033[4;33mUpdating Termux.\033[0m\n"
+sleep 1
+echo -ne "\033[31m>>>>>>>>                                [20%]\033[0m\r"
 apt update > /dev/null 2>&1;
+echo -ne "\033[31m>>>>>>>>>>>>>>>>                        [40%]\033[0m\r"
 yes | apt full-upgrade > /dev/null 2>&1;
+echo -ne "\033[31m>>>>>>>>>>>>>>>>>>>>>>>>                [60%]\033[0m\r"
 pkg update > /dev/null 2>&1;
+echo -ne "\033[31m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        [80%]\033[0m\r"
 yes | pkg upgrade > /dev/null 2>&1;
-echo ${INFO}"Packages updated."${CLR};sleep 0.25s;
+echo -ne "\033[32m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\033[0m\n"
+sleep 1
+echo -ne "\033[1m\033[4;32mTermux has been updated.\033[0m\n"
+sleep 1
 
-# Cleanup
-echo ${INFO}"Cleaning up packages."${CLR};sleep 0.25s;
+# Cleanup Packages
+echo -ne "\033[3m\033[4;33mCleaning up Termux packages.\033[0m\n"
+sleep 1
+echo -ne "\033[31m>>>>>>>>                                [20%]\033[0m\r"
 apt clean > /dev/null 2>&1;
+echo -ne "\033[31m>>>>>>>>>>>>>>>>                        [40%]\033[0m\r"
 apt autoclean > /dev/null 2>&1;
+echo -ne "\033[31m>>>>>>>>>>>>>>>>>>>>>>>>                [60%]\033[0m\r"
 apt autoremove > /dev/null 2>&1;
-pkg clean > /dev/null 2>&1;
-pkg autoclean > /dev/null 2>&1;
-echo ${INFO}"Packages cleaned up."${CLR};sleep 0.25s;
+echo -ne "\033[31m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        [80%]\033[0m\r"
+pkg clean > /dev/null 2>&1 && pkg autoclean > /dev/null 2>&1;
+echo -ne "\033[32m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\033[0m\n"
+sleep 1
+echo -ne "\033[1m\033[4;32mTermux Packages have been cleaned up.\033[0m\n"
 exit
